2c84342c641a3d92fedc18972ee3695f06ad584d
Sebastian-Sinclair <seb.lucas.sinclair@gmail.com>
2024-05-11 18:10:28 -0400

    fixes?

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(express.static('dist'))
 app.use(cors())
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
